11fa72d547c6aa3408adc647277e1a3a4c991baa
IDNatte <35636456+IDNatte@users.noreply.github.com>
2022-07-07 16:46:25 +0800

    Delete interface/layout directory

--- interface/layout/assets/index.3461d85e.js
+++
@@ -1,1 +0,0 @@
-import{g as n}from"./index.c798936e.js";function r(t,{delay:o=0,duration:e=400,easing:i=n}={}){const a=+getComputedStyle(t).opacity;return{delay:o,duration:e,easing:i,css:c=>`opacity: ${c*a}`}}export{r as f};
